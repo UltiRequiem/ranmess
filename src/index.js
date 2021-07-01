6eb8c56e2a6760ad3a48d09ac9ac69c76c68badb
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-07-01 10:27:03 -0500

    :art: Add Comments

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ const fetch = require('node-fetch');
 
 const API = 'https://api.quotable.io/random';
 
+/**
+ * @param {string} apiURl An URL to feth
+ * @returns {Promise<object>} A json
+ */
 async function fetchAndParse(apiURl) {
   const response = await fetch(apiURl);
   return response.json();
 }
 
+/** Run all */
 function init() {
   fetchAndParse(API).then((json) => console.log(json.content));
 }
