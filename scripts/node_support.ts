7b404ec554ab57d3d7784a04f17c4ca7d0b1092b
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-03-21 22:31:50 -0500

    :bookmark: 2.1.7

--- scripts/node_support.ts
+++ scripts/node_support.ts
@@ -1,9 +1,9 @@
 import { buildPackage } from "https://deno.land/x/ultirequiem/node_support.ts";
 
-await buildPackage({
-  repoName: "@ultirequiem/ranmess",
+buildPackage({
+  repoName: "ranmess",
   description: "Quotable Wrapper and CLI Tool.",
   homepage: "https://ranmess.js.org",
   keywords: ["quotes", "quote", "random", "random-quote"],
-  version: "2.1.6",
+  version: "2.1.7",
 }, { supportCJS: false, shims: { undici: true, deno: true } });
