9baf680394767c95bf3943f45c3b180c4435c5d2
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-05-04 15:46:58 -0500

    :recycle: Update

--- mod.ts
+++ mod.ts
@@ -1,4 +1,5 @@
 import { addParams } from "https://deno.land/x/add_params@v0.1.0/mod.ts";
+import type { ItemOf } from "https://deno.land/x/ultirequiem@0.0.13/types.ts";
 
 export const QUOTABLE_TAGS = [
   "business",
@@ -23,7 +24,7 @@ export const QUOTABLE_TAGS = [
   "wisdom",
 ] as const;
 
-export type QuotableTag = typeof QUOTABLE_TAGS[number];
+export type QuotableTag = ItemOf<typeof QUOTABLE_TAGS>;
 
 export interface Quotable {
   _id: string;
